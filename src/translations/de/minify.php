8148ccdcdd9a3af4954b9e56f8a9fcbc0f99cf46
Simon Müller <simon.d.mueller@gmail.com>
2019-10-03 10:38:41 +0200

    german translation

---
+++ src/translations/de/minify.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Minify plugin for Craft CMS 3.x
+ *
+ * @link      https://nystudio107.com/
+ * @copyright Copyright (c) 2017 nystudio107
+ * @license   MIT License https://opensource.org/licenses/MIT
+ */
+/**
+ * @author    nystudio107
+ * @package   Minify
+ * @since     1.2.0
+ */
+return [
+    'Minify plugin loaded' => 'Minify Plugin geladen',
+];
